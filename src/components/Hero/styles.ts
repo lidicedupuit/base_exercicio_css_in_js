3befbc66ded96e0255b6d8ae8e24ed75bfeee72b
lidicedupuit <122185688+lidicedupuit@users.noreply.github.com>
2024-05-09 14:42:02 -0300

    Update styles.ts

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Formulario = styled.form`
+export const Form = styled.form`
   height: 360px;
   width: 100%;
   background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
@@ -15,18 +15,32 @@ export const Formulario = styled.form`
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: var(--cor-principal);
+    background-color: ${(props) => props.theme.corPrincipal};
     content: '';
     opacity: 0.7;
+  }
+
+  div {
+    position: relative;
+    color: ${(props) => props.theme.CorTextoHero};
+    font-family: Gloock, serif;
+    font-size: 28px;
   }
 
   @media (max-width: 768px) {
     height: auto;
     padding: 24px 0;
+
+    div {
+      font-size: 32px;
+    }
   }
 `
-
-export const HeroTitle = styled.h2`
+export const H2Title = styled.h2`
+  font-family: Gloock, serif;
+  font-size: 48px;
+`
+export const DivContainer = styled.h2`
   font-family: Gloock, serif;
   font-size: 48px;
 
@@ -35,7 +49,5 @@ export const HeroTitle = styled.h2`
   }
 `
 
-export const DivContainer = styled.div`
-  position: relative;
-  color: #eee;
+
 `
